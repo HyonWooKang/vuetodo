41110b56d88233727118c2800d1b9b608d2949a3
HyonWooKang <87benzenes@gmail.com>
2023-03-06 22:16:25 +0900

    검색 컴포넌트 추가

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,43 @@
 <template>
   <div>
     <h1>Vue Todo with Typescript</h1>
-    <todoinput :item="todoText"></todoinput>
+    <form action="">
+      <TodoInput 
+      :item="todoText"
+      @input="updateTodoText"
+      @add="addTodoItem">
+    </TodoInput>
+    <!-- 뷰와 컴포넌트 태그에 @input을 이용하여 v-model과 동일한 효과 처리 -->
+    <TodoInput v-model="todoText" @add="addTodoItem">
+    </TodoInput>
+    </form>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import Todoinput from "./components/Todoinput.vue";
+import TodoInput from "./components/Todoinput.vue";
 
 export default Vue.extend({
-  components: { Todoinput },
+  components: { TodoInput },
   data() {
     return {
-      todoText: ""
+      todoText: ""  // 단 방향 바인딩 성공 App.vue -> Todoinput
     };
+  },
+  methods: {
+    updateTodoText(value: string) {
+
+      this.todoText = value;
+    },
+    addTodoItem() {
+      const value = this.todoText;
+      localStorage.setItem(value, value);
+      this.initTodoText();
+    },
+    initTodoText() {
+      this.todoText = "";
+    }
   }
 });
 </script>
--- src/components/Todoinput.vue
+++ src/components/Todoinput.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <label for="todo-input"></label>
-    <input id="todo-input" type="text" :value="item"/>
+    <label for="todo-input">오늘 할 일 : </label>
+    <input id="todo-input" type="text" :value="item" @input="handleInput"/>
     <button @click="addTodo" type="button">add</button>
   </div>
 </template>
@@ -10,10 +10,27 @@
 import Vue from "vue";
 
 export default Vue.extend({
-  props: ["item"],
+  // props: ["item"],
+  props: {
+    item: {
+      // 자바스크립트의 input 속성과 동일
+      type: String,
+      required: true, // 필수 값
+    }
+  },
   methods: {
+    handleInput(event: InputEvent) {  // 기존에는 any로 처리했음
+      console.log("event")
+      /*
+      이벤트 타겟이 null 일 수 있다는 에러 발생 => null 체크 가능
+      이벤트 타겟에 value 값이 없다는 에러 발생
+      => 두 개를 모두 해결하기 위해서는 안정도는 떨어지지만 이렇게 타입 정의를 해서 해결 할 수 있다.
+       */
+      const eventTarget = event.target as HTMLInputElement
+      this.$emit('input', eventTarget.value);
+    },
     addTodo() {
-      console.log("add");
+      this.$emit("add");
     }
   }
 
