e93676014fe976cdfc9cc783e0e0b8f6d8c6ce1f
HyonWooKang <87benzenes@gmail.com>
2023-03-07 12:16:29 +0900

    입력한 내용을 로컬 스토리지에서 배열 형태로 저장하고 스트링 형태로 사용하기

--- src/App.vue
+++ src/App.vue
@@ -33,10 +33,11 @@ import TodoListItem from "./components/TodoListItem.vue";
 const STORAGE_KEY = "vue-todo-ts-v1"
 const storage = {
   // 값 저장
-  save() {
-    
+  save(todoItems: any) {
+    const parsed = JSON.stringify(todoItems); // 배열을 스트링으로 파싱
+    localStorage.setItem(STORAGE_KEY, parsed);
   },
-  // 값 보내주기
+  // 값 로컬 스토리지로 보내주기
   fetch() {
     const todoItems = localStorage.getItem(STORAGE_KEY) || "[]";  // 값이 없으면 초기화 시켜줌
     const result = JSON.parse(todoItems);  // JSON(String)을 Object형태로 변경
@@ -49,7 +50,7 @@ export default Vue.extend({
   data() {
     return {
       todoText: "",  // 단 방향 바인딩 성공 App.vue -> Todoinput
-      todoItems: [],  // localStroage에 있는 데이터를 
+      todoItems: [] as any[],  // localStroage에 있는 데이터를 
     };
   },
   methods: {
@@ -59,7 +60,9 @@ export default Vue.extend({
     },
     addTodoItem() {
       const value = this.todoText;
-      localStorage.setItem(value, value); // 웹 애플리케이션 로컬스토리지에 저장
+      //localStorage.setItem(value, value); // 웹 애플리케이션 로컬스토리지에 저장
+      this.todoItems.push(value); // 배열로 된 값을 집어 넣기
+      storage.save(this.todoItems);
       this.initTodoText();  // 입력창 초기화
     },
     initTodoText() {
